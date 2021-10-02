a723f2edf96051b625f00121633d879e3069215c
Documenter.jl <documenter@juliadocs.github.io>
2021-10-02 22:51:48 +0000

    build based on 64ed064

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SIMDDualNumbers","category":"page"},{"location":"#SIMDDualNumbers","page":"Home","title":"SIMDDualNumbers","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SIMDDualNumbers.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SIMDDualNumbers]","category":"page"}]
+}
